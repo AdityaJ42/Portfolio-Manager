bc46de8ece770be0d81408d0f735e8feed970afe
mustafabhanpura <mustafatasneem53@gmail.com>
2019-03-02 17:57:10 +0530

    changes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-# from django.db import models
-#
-# # Create your models here.
-# class SignUp(models.Model):
-#     username=models.CharField(max_length=50,blank=True)
-#     email= models.EmailField()
-#
-#     def __str__(self):
-#         return self.username
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    company_name = models.CharField(max_length=50,blank=True)
+    company_intial = models.CharField(max_lenght=10,blank=True)
+
+    def __str__(self):
+        return self.company_name
